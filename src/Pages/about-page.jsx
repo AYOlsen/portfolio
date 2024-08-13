8d31577f502518b507984427b1bf870bf065c7f2
AYOlsen <olsenannayodit06@gmail.com>
2024-08-13 23:22:22 +0200

    Make responsive

--- src/Pages/about-page.jsx
+++ src/Pages/about-page.jsx
@@ -3,9 +3,9 @@ export default function AboutPage () {
     return (
       <>
         <section className="bg-background">
-          <div className='max-width mx-auto py-32 flex justify-between'>
-            <div className='w-2/5 text-brown-200 about-text'>
-              <h2 className='text-brown-200 mb-8'>About me</h2>
+          <div className='max-width mx-auto py-8 md:py-32 md:flex md:justify-between px-7'>
+            <div className='md:w-2/5 text-brown-200 about-text'>
+              <h2 className='text-brown-200 mb-8 font-bold text-xl'>About me</h2>
               <p>
                 Hello, I'm Anna-Yodit Olsen, a recently graduated front-end developer. My expertise includes HTML, CSS, and JavaScript.
               </p>
@@ -15,7 +15,7 @@ export default function AboutPage () {
                 Explore my portfolio and check out some of my recent projects.
               </p>
             </div>
-            <div className="about-image w-[30rem]">
+            <div className="about-image md:w-[30rem]">
               <img src={process.env.PUBLIC_URL + '/images/about-me.png'} alt="Cartoon of myself" />
             </div>
           </div>
